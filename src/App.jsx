cd316adfb2978c612c1d061fac6605e29df1dde4
rutvijjoshi26 <rutvijjoshi5@gmail.com>
2023-04-26 14:52:20 +0530

    commit operation

---
+++ src/App.jsx
@@ -0,0 +1,342 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
+import Loader from "./components/loader";
+import ButtonHandler from "./components/btn-handler";
+import "./style/App.css";
+import labelsEnglish from "../src/utils/labelsEnglish.json";
+import labelsHindi from "../src/utils/labelsHindi.json";
+
+
+//renderBox.js
+/**
+ * Render prediction boxes
+ * @param {HTMLCanvasElement} canvasRef canvas tag reference
+ * @param {number} classThreshold class threshold
+ * @param {Array} boxes_data boxes array
+ * @param {Array} scores_data scores array
+ * @param {Array} classes_data class array
+ * @param {Array[Number]} ratios boxes ratio [xRatio, yRatio]
+ */
+
+
+const renderBoxes = (
+  canvasRef,
+  classThreshold,
+  boxes_data,
+  scores_data,
+  classes_data,
+  ratios, modelName
+) => {
+
+  const ctx = canvasRef.getContext("2d");
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+  
+
+  const colors = new Colors();
+  var boundingLabels;
+    if (modelName==="hindi") {
+      boundingLabels = labelsHindi;
+  } else {
+      boundingLabels = labelsEnglish;
+  }
+
+  
+  // font configs
+  const font = `${Math.max(
+    Math.round(Math.max(ctx.canvas.width, ctx.canvas.height) / 40),
+    14
+  )}px Arial`;
+  ctx.font = font;
+  ctx.textBaseline = "top";
+
+  for (let i = 0; i < scores_data.length; ++i) {
+    // filter based on class threshold
+    if (scores_data[i] > classThreshold) {
+      const klass = boundingLabels[classes_data[i]];
+      const color = colors.get(classes_data[i]);
+      const score = (scores_data[i] * 100).toFixed(1);
+
+      let [x1, y1, x2, y2] = boxes_data.slice(i * 4, (i + 1) * 4);
+      x1 *= canvasRef.width * ratios[0];
+      x2 *= canvasRef.width * ratios[0];
+      y1 *= canvasRef.height * ratios[1];
+      y2 *= canvasRef.height * ratios[1];
+      const width = x2 - x1;
+      const height = y2 - y1;
+
+      // draw box.
+      ctx.fillStyle = Colors.hexToRgba(color, 0.2);
+      ctx.fillRect(x1, y1, width, height);
+      // draw border box.
+      ctx.strokeStyle = color;
+      ctx.lineWidth = Math.max(Math.min(ctx.canvas.width, ctx.canvas.height) / 200, 2.5);
+      ctx.strokeRect(x1, y1, width, height);
+
+      // Draw the label background.
+      ctx.fillStyle = color;
+      const textWidth = ctx.measureText(klass + " - " + score + "%").width;
+      const textHeight = parseInt(font, 10); // base 10
+      const yText = y1 - (textHeight + ctx.lineWidth);
+      ctx.fillRect(
+        x1 - 1,
+        yText < 0 ? 0 : yText, // handle overflow label box
+        textWidth + ctx.lineWidth,
+        textHeight + ctx.lineWidth
+      );
+
+      // Draw labels
+      ctx.fillStyle = "#ffffff";
+      ctx.fillText(klass + " - " + score + "%", x1 - 1, yText < 0 ? 0 : yText);
+    }
+  }
+};
+
+class Colors {
+  // ultralytics color palette https://ultralytics.com/
+  constructor() {
+    this.palette = [
+      "#FF3838",
+      "#FF9D97",
+      "#FF701F",
+      "#FFB21D",
+      "#CFD231",
+      "#48F90A",
+      "#92CC17",
+      "#3DDB86",
+      "#1A9334",
+      "#00D4BB",
+      "#2C99A8",
+      "#00C2FF",
+      "#344593",
+      "#6473FF",
+      "#0018EC",
+      "#8438FF",
+      "#520085",
+      "#CB38FF",
+      "#FF95C8",
+      "#FF37C7",
+    ];
+    this.n = this.palette.length;
+  }
+
+  get = (i) => this.palette[Math.floor(i) % this.n];
+
+  static hexToRgba = (hex, alpha) => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? `rgba(${[parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)].join(
+          ", "
+        )}, ${alpha})`
+      : null;
+  };
+}
+
+
+/**
+ * Preprocess image / frame before forwarded into the model
+ * @param {HTMLVideoElement|HTMLImageElement} source
+ * @param {Number} modelWidth
+ * @param {Number} modelHeight
+ * @returns input tensor, xRatio and yRatio
+ */
+const preprocess = (source, modelWidth, modelHeight) => {
+  let xRatio, yRatio; // ratios for boxes
+
+  const input = tf.tidy(() => {
+    const img = tf.browser.fromPixels(source);
+
+    // padding image to square => [n, m] to [n, n], n > m
+    const [h, w] = img.shape.slice(0, 2); // get source width and height
+    const maxSize = Math.max(w, h); // get max size
+    const imgPadded = img.pad([
+      [0, maxSize - h], // padding y [bottom only]
+      [0, maxSize - w], // padding x [right only]
+      [0, 0],
+    ]);
+
+    xRatio = maxSize / w; // update xRatio
+    yRatio = maxSize / h; // update yRatio
+
+    return tf.image
+      .resizeBilinear(imgPadded, [modelWidth, modelHeight]) // resize frame
+      .div(255.0) // normalize
+      .expandDims(0); // add batch
+  });
+
+  return [input, xRatio, yRatio];
+};
+
+/**
+ * Function to detect image.
+ * @param {HTMLImageElement} imgSource image source
+ * @param {tf.GraphModel} model loaded YOLOv5 tensorflow.js model
+ * @param {Number} classThreshold class threshold
+ * @param {HTMLCanvasElement} canvasRef canvas reference
+ */
+const detectImage = async (imgSource, model, classThreshold, canvasRef, modelName) => {
+  const [modelWidth, modelHeight] = model.inputShape.slice(1, 3); // get model width and height
+
+  tf.engine().startScope(); // start scoping tf engine
+  const [input, xRatio, yRatio] = preprocess(imgSource, modelWidth, modelHeight);
+
+  await model.net.executeAsync(input).then((res) => {
+    const [boxes, scores, classes] = res.slice(0, 3);
+    const boxes_data = boxes.dataSync();
+    const scores_data = scores.dataSync();
+    const classes_data = classes.dataSync();
+    renderBoxes(canvasRef, classThreshold, boxes_data, scores_data, classes_data, [xRatio, yRatio], modelName); // render boxes
+    tf.dispose(res); // clear memory
+  });
+
+  tf.engine().endScope(); // end of scoping
+};
+
+/**
+ * Function to detect video from every source.
+ * @param {HTMLVideoElement} vidSource video source
+ * @param {tf.GraphModel} model loaded YOLOv5 tensorflow.js model
+ * @param {Number} classThreshold class threshold
+ * @param {HTMLCanvasElement} canvasRef canvas reference
+ */
+const detectVideo = (vidSource, model, classThreshold, canvasRef, modelName) => {
+  const [modelWidth, modelHeight] = model.inputShape.slice(1, 3); // get model width and height
+
+  /**
+   * Function to detect every frame from video
+   */
+  const detectFrame = async () => {
+    if (vidSource.videoWidth === 0 && vidSource.srcObject === null) {
+      const ctx = canvasRef.getContext("2d");
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+      return; // handle if source is closed
+    }
+
+    tf.engine().startScope(); // start scoping tf engine
+    const [input, xRatio, yRatio] = preprocess(vidSource, modelWidth, modelHeight);
+
+    await model.net.executeAsync(input).then((res) => {
+      const [boxes, scores, classes] = res.slice(0, 3);
+      const boxes_data = boxes.dataSync();
+      const scores_data = scores.dataSync();
+      const classes_data = classes.dataSync();
+      renderBoxes(canvasRef, classThreshold, boxes_data, scores_data, classes_data, [
+        xRatio,
+        yRatio, modelName
+      ]); // render boxes
+      tf.dispose(res); // clear memory
+    });
+
+    requestAnimationFrame(detectFrame); // get another frame
+    tf.engine().endScope(); // end of scoping
+  };
+
+  detectFrame(); // initialize to detect every frame
+};
+
+
+
+
+
+const App = () => {
+  // const [isClicked, setIsCliked] = useState(false);
+  const [loading, setLoading] = useState({ loading: true, progress: 0 }); // loading state
+  const [model, setModel] = useState({
+    net: null,
+    inputShape: [1, 0, 0, 3],
+  }); // init model & input shape
+
+  // references
+  const imageRef = useRef(null);
+  const cameraRef = useRef(null);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // model configs
+  const  [modelName, setModelName] = useState("english");
+  const classThreshold = 0.2;
+
+  const handleChange = (event) => {
+
+    setModelName(event.target.value);
+ 
+  };
+
+  useEffect(() => {
+    tf.ready().then(async () => {
+      const yolov5 = await tf.loadGraphModel(
+        `${window.location.origin}/${modelName}_web_model/model.json`,
+        {
+          onProgress: (fractions) => {
+            setLoading({ loading: true, progress: fractions }); // set loading fractions
+          },
+        }
+      ); // load model
+
+      // warming up model
+      const dummyInput = tf.ones(yolov5.inputs[0].shape);
+      const warmupResult = await yolov5.executeAsync(dummyInput);
+      tf.dispose(warmupResult); // cleanup memory
+      tf.dispose(dummyInput); // cleanup memory
+
+      setLoading({ loading: false, progress: 1 });
+      setModel({
+        net: yolov5,
+        inputShape: yolov5.inputs[0].shape,
+      }); // set model & input shape
+    });
+  }, [modelName]);
+
+  return (
+    <div className="App">
+      {loading.loading && <Loader>Loading model... {(loading.progress * 100).toFixed(2)}%</Loader>}
+      <div className="header">
+        <h1>📷 YOLOv5 Live Detection App</h1>
+        <p>
+          YOLOv5 live detection application on browser powered by <code>tensorflow.js</code>
+        </p>
+        <p>
+          Serving : <code className="code">{modelName}</code>
+        </p>
+      </div>
+
+      <div className="content">
+        <img
+          src="#"
+          ref={imageRef}
+          onLoad={() => detectImage(imageRef.current, model, classThreshold, canvasRef.current, modelName)}
+        />
+        <video
+          autoPlay
+          muted
+          ref={cameraRef}
+          onPlay={() => detectVideo(cameraRef.current, model, classThreshold, canvasRef.current, modelName)}
+        />
+        <video
+          autoPlay
+          muted
+          ref={videoRef}
+          onPlay={() => detectVideo(videoRef.current, model, classThreshold, canvasRef.current, modelName)}
+        />
+        <canvas width={model.inputShape[1]} height={model.inputShape[2]} ref={canvasRef} />
+      </div>
+
+      <ButtonHandler imageRef={imageRef} cameraRef={cameraRef} videoRef={videoRef} />
+      {/* <button onClick={() => {setModelName("hindi")}}>Change Model</button>  */}
+      <label>
+
+      Select Language
+       <select value={modelName} onChange={handleChange}>
+
+         <option value="english">English</option>
+
+         <option value="hindi">Hindi</option>
+
+       </select>
+
+     </label>
+    </div>
+  );
+};
+
+export default App;
